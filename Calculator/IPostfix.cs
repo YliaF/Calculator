1387f1ef5952ec3b320209d098e46ee9fa96ad3f
ylia.fatk@gmail.com <tT7oiI0h_1b4GefD>
2015-03-04 11:54:02 +0600

    add interfaces

---
+++ Calculator/IPostfix.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public interface IPostfix
+    {
+        void CreatePostfixExpression(string inputExpression);
+        void Counting();
+
+        IList<string> PostfixExpression { get; set; }
+
+        double CountedResult { get; set; }
+    }
+}
