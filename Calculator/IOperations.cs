99fb21f8e2f4cf5d803f28491403cfcad3aa0dac
ylia.fatk@gmail.com <tT7oiI0h_1b4GefD>
2015-03-16 23:58:55 +0600

    commit

---
+++ Calculator/IOperations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public interface IOperations
+    {
+        IList<string> OperatorList { get; set; }
+
+        Dictionary<string, int> PriorityOperations { get; set; }
+
+        Dictionary<string, Func<double, double, double>> BinaryOperations { get; set; }
+
+        Dictionary<string, Func<double, double>> UnaryOperations { get; set; }
+
+        void AddBinaryOperation(string operation, Func<double, double, double> action);
+
+        void AddUnaryOperation(string operation, Func<double, double> action);
+
+        void SetPriorityOperations(string operation, int priority);
+    }
+}
