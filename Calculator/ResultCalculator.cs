20ac35006022cd2fdb323004adc046ff0c3bf891
ylia.fatk@gmail.com <tT7oiI0h_1b4GefD>
2015-03-20 20:12:46 +0600

    Результат вычислений постфиксного выражения вынесен в отдельный класс

---
+++ Calculator/ResultCalculator.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Calc
+{
+    public class ResultCalculator : IResultCalculator<double>
+    {
+        private double resultCalculator;
+        public double Result
+        {
+            get
+            {
+                return resultCalculator;
+            }
+            set
+            {
+                resultCalculator = value;
+            }
+        }
+    }
+}
