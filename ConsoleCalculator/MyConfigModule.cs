d7cef87d831ca3350ac4a3a6efecb71038fbb481
ylia.fatk@gmail.com <tT7oiI0h_1b4GefD>
2015-03-21 00:44:23 +0600

    IoC-контейнер ninject

---
+++ ConsoleCalculator/MyConfigModule.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Ninject.Modules;
+using Calc;
+
+namespace ConsoleCalculator
+{
+    public class MyConfigModule: NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IOperations>().To<Operations>();
+            Bind<IParser<string[]>>().To<Parser>();
+            Bind<IParsedExpression<string[]>>().To<ParsedExpression>();
+            Bind<IPostfix<IList<string>>>().To<Postfix>();
+            Bind<IPostfixExpression<IList<string>>>().To<PostfixExpression>();
+            Bind<ICalculatorPostfix>().To<CalculatorPostfix>();
+            Bind<IResultCalculator<double>>().To<ResultCalculator>();
+            
+        }
+    }
+}
