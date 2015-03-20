0fe8ec20a3e2cb27f009c59fd37d78be4b4c374e
ylia.fatk@gmail.com <tT7oiI0h_1b4GefD>
2015-03-21 00:40:21 +0600

    ConsoleCalculator использует IoC-контейнер Ninject для инверсии зависимостей (пока неполная)

--- ConsoleCalculator/ConsoleCalculator.cs
+++ ConsoleCalculator/ConsoleCalculator.cs
@@ -1,14 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Ninject;
 using Calc;
+using System.Collections.Generic;
 
 namespace ConsoleCalculator
 {
-    class ConsoleCalculator
+    public class ConsoleCalculator : ICalculator
     {
+        private IKernel kernel = new StandardKernel(new MyConfigModule());
 
+        public double Calculate(string inputExpression)
+        {
+            IOperations operations = kernel.Get<Operations>();
+
+            //Парсинг
+            IParsedExpression<string[]> parsedExpression = kernel.Get<ParsedExpression>();
+            IParser<string[]> parser = new Parser(operations);
+            parsedExpression.Result = parser.Parse(inputExpression);
+
+            //Постфикс
+            IPostfixExpression<IList<string>> postfixExpression = kernel.Get<PostfixExpression>();
+            IPostfix<IList<string>> postfix = new Postfix(operations);
+            postfixExpression.Result = postfix.ConvertToPostfix(parsedExpression);
+
+            //Решение постфикса
+            IResultCalculator<double> resultCalculator = kernel.Get<ResultCalculator>();
+            ICalculatorPostfix calculatorPostfix = new CalculatorPostfix(operations);
+            resultCalculator.Result = calculatorPostfix.Calculate(postfixExpression);
+
+            return resultCalculator.Result;
+        }
     }
 }
