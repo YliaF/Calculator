fb92e8267d487ede65f06ec75cee75db6e82032b
ylia.fatk@gmail.com <tT7oiI0h_1b4GefD>
2015-03-20 20:23:41 +0600

    Интерфейс вычисления постфиксного выражения

---
+++ Calculator/ICalculatorPostfix.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Calc
+{
+    public interface ICalculatorPostfix
+    {
+        void Calculate(IPostfixExpression<IList<string>> postfixExpression, IResultCalculator<double> resultCalculator);
+
+        double Calculate(IPostfixExpression<IList<string>> postfixExpression);
+    }
+}
