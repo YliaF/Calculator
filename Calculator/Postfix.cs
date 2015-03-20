e1457b52a58f510738a0e20d3fb3496bc14832ae
ylia.fatk@gmail.com <tT7oiI0h_1b4GefD>
2015-03-20 20:19:02 +0600

    Переименован класс конвертации в постфиксную запись, вынесены поля в отдельные классы

---
+++ Calculator/Postfix.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Calc
+{
+    public class Postfix : IPostfix<IList<string>>
+    {
+        private IOperations operations;
+
+        public IOperations Operations
+        {
+            get { return operations; }
+            set { operations = value; }
+        }
+
+        public Postfix(IOperations operations)
+        {
+            Operations = operations;
+        }
+        public void ConvertToPostfix(IParsedExpression<string[]> parsedExpression, IPostfixExpression<IList<string>> postfixExpression)
+        {
+            IList<string> postfix = new List<string>();
+
+            Stack<string> stackOperators = new Stack<string>();
+            foreach (string token in parsedExpression.Result)
+            {
+                if (token == "(")
+                    stackOperators.Push(token);
+                else if (IsOperation(token))
+                {
+                    while ((stackOperators.Count != 0) && (Priority(stackOperators.Peek()) >= Priority(token)))
+                    {
+                        postfix.Add(stackOperators.Pop());
+                    }
+                    stackOperators.Push(token);
+                }
+                else if (token == ")")
+                {
+                    while ((stackOperators.Count() != 0) && (stackOperators.Peek() != "("))
+                    {
+                        postfix.Add(stackOperators.Pop());
+                    }
+                    if (stackOperators.Count() == 0)
+                        throw new Exception("This expression is incorrect.\n");
+                    stackOperators.Pop();
+                }
+                else
+                {
+                    postfix.Add(token);
+                }
+            }
+            while (stackOperators.Count() != 0)
+            {
+                postfix.Add(stackOperators.Pop());
+            }
+            postfixExpression.Result = postfix;
+        }
+        public IList<string> ConvertToPostfix(IParsedExpression<string[]> parsedExpression)
+        {
+            IList<string> postfix = new List<string>();
+
+            Stack<string> stackOperators = new Stack<string>();
+            foreach (string token in parsedExpression.Result)
+            {
+                if (token == "(")
+                    stackOperators.Push(token);
+                else if (IsOperation(token))
+                {
+                    while ((stackOperators.Count != 0) && (Priority(stackOperators.Peek()) >= Priority(token)))
+                    {
+                        postfix.Add(stackOperators.Pop());
+                    }
+                    stackOperators.Push(token);
+                }
+                else if (token == ")")
+                {
+                    while ((stackOperators.Count() != 0) && (stackOperators.Peek() != "("))
+                    {
+                        postfix.Add(stackOperators.Pop());
+                    }
+                    if (stackOperators.Count() == 0)
+                        throw new Exception("This expression is incorrect.\n");
+                    stackOperators.Pop();
+                }
+                else
+                {
+                    postfix.Add(token);
+                }
+            }
+            while (stackOperators.Count() != 0)
+            {
+                postfix.Add(stackOperators.Pop());
+            }
+            return postfix;
+        }
+        private int Priority(string token)
+        {
+            try
+            {
+                return Operations.PriorityOperations[token];
+            }
+            catch (KeyNotFoundException e)
+            {
+                throw new KeyNotFoundException("For operation ' " + token + " ' is not set priority.\n", e);
+            }
+
+        }
+
+        private bool IsOperation(string token)
+        {
+            return Operations.OperatorList.Contains(token);
+        }
+
+
+    }
+}
