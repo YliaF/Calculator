b5a458eb198d478492ffc5f956cc7f3174dae07e
ylia.fatk@gmail.com <tT7oiI0h_1b4GefD>
2015-03-20 20:13:44 +0600

    Постфиксное выражение вынесено в отдельный класс

---
+++ Calculator/PostfixExpression.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Calc
+{
+    public class PostfixExpression : IPostfixExpression<IList<string>>
+    {
+        private IList<string> postfixExpression = new List<string>();
+        public IList<string> Result
+        {
+            get
+            {
+                return postfixExpression;
+            }
+            set
+            {
+                postfixExpression = value;
+            }
+        }
+    }
+}
