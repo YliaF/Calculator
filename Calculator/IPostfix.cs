b82bc970f1dc56d36a541f5411a57f2a05dbeb8c
ylia.fatk@gmail.com <tT7oiI0h_1b4GefD>
2015-03-22 15:11:55 +0600

    Удалено описание  лишнего метода

--- Calculator/IPostfix.cs
+++ Calculator/IPostfix.cs
@@ -4,7 +4,5 @@ namespace Calc
     public interface IPostfix<T>
     {
         T ConvertToPostfix(IParsedExpression<string[]> parsedExpression);
-
-        void ConvertToPostfix(IParsedExpression<string[]> parsedExpression, IPostfixExpression<T> postfixExpression);
     }
 }
