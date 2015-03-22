7e262bab0afed592ebce84a7fef6e5780204f0bd
ylia.fatk@gmail.com <tT7oiI0h_1b4GefD>
2015-03-22 15:14:16 +0600

    Удалена реализация лишнего метода

--- Calculator/CalculatorPostfix.cs
+++ Calculator/CalculatorPostfix.cs
@@ -18,60 +18,6 @@ namespace Calc
         {
             Operations = operations;
         }
-
-        public void Calculate(IPostfixExpression<IList<string>> postfixExpression, IResultCalculator<double> resultCalculator)
-        {
-            Stack<double> stackNumber = new Stack<double>();
-            double first, second;
-            foreach (string token in postfixExpression.Result.ToArray())
-            {
-                double res = 0;
-                if (IsUnaryMinus(token) && stackNumber.Count() != 0)
-                {
-                    first = -stackNumber.Pop();
-                    stackNumber.Push(first);
-                }
-                else if (!IsOperation(token))
-                    try
-                    {
-                        stackNumber.Push(Convert.ToDouble(token));
-                    }
-                    catch (FormatException e)
-                    {
-                        throw new FormatException("The expression contains an invalid character '" + token + "'.\n", e);
-                    }
-
-                else
-                {
-                    try
-                    {
-                        second = stackNumber.Pop();
-                        first = stackNumber.Pop();
-                    }
-                    catch (Exception e)
-                    {
-                        throw new Exception("This expression is incorrect.\n", e);
-                    }
-
-                    try
-                    {
-                        res = ApplyOperations(first, second, token);
-                    }
-                    catch (Exception e)
-                    {
-                        throw new Exception(e.Message, e);
-                    }
-                    finally
-                    {
-                        if (Double.IsInfinity(res))
-                            throw new DivideByZeroException("Divide by zero exception.\n");
-                    }
-                    stackNumber.Push(res);
-                }
-            }
-            resultCalculator.Result = stackNumber.Peek();
-        }
-
         public double Calculate(IPostfixExpression<IList<string>> postfixExpression)
         {
             Stack<double> stackNumber = new Stack<double>();
